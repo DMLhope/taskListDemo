06fef651d28876e1a5d99204fe2444de12ad2091
DML <605811524@qq.com>
2020-02-22 23:08:26 +0800

    更新本地存储，本地删除功能

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ function loadEventListeners() {
     clearBtn.addEventListener('click', clearTasks);
     //过滤任务
     filter.addEventListener('keyup', filterTasks);
+    // 加载完所用dom内容时候就加载本地已保存的数据
+    document.addEventListener('DOMContentLoaded', getTask);
+
 }
 
 //addTask
@@ -49,6 +52,9 @@ function addTask(e) {
         li.appendChild(link);
         // 将li插入ul当中
         taskList.appendChild(li);
+
+        // 将添加的任务进行本地存储
+        storeTaskInLocalStorage(taskInput.value);
         // 添加完成后清空输入框
         taskInput.value = "";
     }
@@ -64,26 +70,32 @@ function removeTask(e) {
     if (e.target.parentElement.classList.contains('delete-item')) {
         if (confirm('是否确认完成？')) {
             e.target.parentElement.parentElement.remove();
+            //删除本地存储任务
+            removeTaskFormLoaclStorage(e.target.parentElement.parentElement);
         }
     }
+
 }
 
 // clearTasks
 function clearTasks(e) {
-    // 方法1
-    // taskList.innerHTML = "";
-    //方法2
-    // 如果ul有第一个子元素则删除第一个子元素，一直循环
-    while (taskList.firstChild) {
-        taskList.removeChild(taskList.firstChild);
+    if (confirm('是否确认删除所有任务？')) {
+        // 方法1
+        // taskList.innerHTML = "";
+        //方法2
+        // 如果ul有第一个子元素则删除第一个子元素，一直循环
+        while (taskList.firstChild) {
+            taskList.removeChild(taskList.firstChild);
+        }
+        // 删除所有任务并保存本地
+        removeAlltasksFormLocalStorage();
+        //方法3  自己想讨巧的方法会有问题，无法再添加任务因为整个ul都被删掉了不存在了
+        // if (confirm('是否确认清除？')) {
+        //     taskList.remove();
+        // }
     }
-    //方法3  自己想讨巧的方法会有问题，无法再添加任务因为整个ul都被删掉了不存在了
-    // if (confirm('是否确认清除？')) {
-    //     taskList.remove();
-    // }
-
 }
-
+//过滤任务函数
 function filterTasks(e) {
     // 获取用户输入内容 并转换小写
     const text = e.target.value.toLowerCase();;
@@ -106,5 +118,87 @@ function filterTasks(e) {
             }
         }
     );
+}
 
+// 本地存储函数
+function storeTaskInLocalStorage(task) {
+    // 创建tasks的变量
+    let tasks;
+    // 从本地存储中判断tasks是否为空，如果为空则创建tasks为空数组
+    if (localStorage.getItem('tasks') == null) {
+        tasks = [];
+    } else {
+        // 若不为空则将本地存储的值赋值给tasks 用json转换下值类型为数组
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    // 将传入的task添加到tasks中
+    tasks.push(task);
+    // 进行本地存储，设置键名为tasks，因为传入的值得是string所以用json的方法将数组转为string
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+//getTask 获取本地存储的任务
+function getTask() {
+    // 创建tasks的变量
+    let tasks;
+    // 从本地存储中判断tasks是否为空，如果为空则创建tasks为空数组
+    if (localStorage.getItem('tasks') == null) {
+        tasks = [];
+    } else {
+        // 若不为空则将本地存储的值赋值给tasks 用json转换下值类型为数组
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    // console.log(tasks);
+    // 通过foreach来进行循环的遍历
+    tasks.forEach(function(task) {
+        // 创建节点li
+        const li = document.createElement('li');
+        //添加li类名
+        li.className = "collection-item";
+        // 创建文本节点，插入li中
+        li.appendChild(document.createTextNode(task));
+        //创建a标签来添加删除的图标
+        const link = document.createElement('a');
+        //添加类名，这样才有对应的样式
+        link.className = 'delete-item secondary-content';
+        //添加字体图标
+        link.innerHTML = '<i class="fa fa-times"></i>';
+        // 将a标签插入li中
+        li.appendChild(link);
+        // 将li插入ul当中
+        taskList.appendChild(li);
+    })
+}
+
+// 从本地存储中删除目标任务
+function removeTaskFormLoaclStorage(taskItem) {
+    // console.log(taskItem);
+    // 创建tasks的变量
+    let tasks;
+    // 从本地存储中判断tasks是否为空，如果为空则创建tasks为空数组
+    if (localStorage.getItem('tasks') == null) {
+        tasks = [];
+    } else {
+        // 若不为空则将本地存储的值赋值给tasks 用json转换下值类型为数组
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    // 通过判断被删除的tasks的值在本地存储中是否存在来选择是否更新本地存储
+    tasks.forEach(function(task, index) {
+        if (taskItem.textContent === task) {
+            // splice方法第一个参数是表示从哪一个下标开始删除，第二个参数是表示删除几个
+            tasks.splice(index, 1);
+            console.log(tasks);
+            // 将tasks更新到本地存储
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+        }
+    })
+}
+
+// 从本地存储中删除所有任务
+function removeAlltasksFormLocalStorage(taskItem) {
+    // 智障的想用一个空的数组更新掉之前的数组但其实只需要一步
+    // let tasks;
+    // tasks = [];
+    // localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.clear();
 }
